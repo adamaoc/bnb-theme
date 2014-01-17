242219a88143ffbdfc04b10338fa3256187ab13d
adamaoc <adamaoc@gmail.com>
2014-01-16 19:41:04 -0600

    added next and prev arrows to featured photography

--- _/single/featured-single.php
+++ _/single/featured-single.php
@@ -1,10 +1,18 @@
 <?php 
 // Single page for featured photography
 	$photo = get_field('photo'); 
+	// print_r(get_field('photo'));
 ?>
 <h1><?php the_title(); ?></h1>
 <section class="photo-wrapper" data-width="<?php echo $photo['sizes']['large-width']; ?>">
 	<img src="<?php echo $photo['sizes']['large']; ?>" alt="<?php the_title(); ?>" />
+	<?php 
+
+		$nexlink = '<img src="'.get_bloginfo('template_directory').'/_/img/arrow_RightLight.png">';
+		$prevlink = '<img src="'.get_bloginfo('template_directory').'/_/img/arrow_LeftLight.png">';
+		previous_post_link('%link', $prevlink);
+		next_post_link('%link', $nexlink);
+	?>
 </section>
 
 <script>
