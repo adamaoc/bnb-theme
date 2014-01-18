9e2c051b46a20078d8235725dce9fb39a327588d
adamaoc <adamaoc@gmail.com>
2014-01-18 12:40:29 -0600

    updated sidebar to show testimonials on Blog Page

--- sidebar.php
+++ sidebar.php
@@ -4,5 +4,37 @@
         <h2>Quick Contact</h2>
         <?php include "_/inc/contactform.php"; ?>
     </section>
-
+<?php if(!is_page('about', 'contact')) { ?>
+    <section class="testimonials">
+		<h2>Testimonials</h2>
+		
+		<?php 
+			$i = 0;
+			$testimonials_query = new WP_Query(array(
+				'post_type' => 'testimonials'
+			));
+			
+			while ($testimonials_query->have_posts()) : 
+				$testimonials_query->the_post(); 		
+				$featured = get_field('featue');
+				
+				if (isset($featured)) { 
+					if ($featured[0] === 'featured') {
+					// only return top 4 featured
+						if ($i < 4) {
+							?>
+							<blockquote>
+								<h3><?php the_field('customer_name'); ?></h3>
+								<time><?php the_field('date'); ?></time>
+								<p><?php the_field('text'); ?></p>
+							</blockquote>
+							<?php
+							$i++; 										
+						}; 
+					};
+				};
+			endwhile; 
+		wp_reset_postdata(); ?>
+	</section>
+<?php } ?>
 </div>
