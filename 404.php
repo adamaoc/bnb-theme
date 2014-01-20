bc2f8df3429296901e49bc10371e41b94b895f58
adamaoc <adamaoc@gmail.com>
2014-01-20 14:52:04 -0600

    updated 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
-
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
-
-<?php get_sidebar(); ?>
-
+<div class="page" style="padding-top: 15px;">
+	<div class="post">
+		<h1><?php _e('Error 404 - Page Not Found','html5reset'); ?></h1>
+		<p>Ooops... I'm sorry, it doesn't look like that page exists. Maybe try heading over to the <a href="/">homepage</a> and see if you can find what you're looking for!</p>
+		<p>Thanks for checking out DFW Bellies and Babies</p>
+	</div>
+	<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
